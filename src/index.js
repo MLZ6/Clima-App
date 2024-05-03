40ea9a39414be596b1f84058d0ca0f8afcf70c8a
MLZ6 <kopomasuga64@gmail.com>
2024-05-03 20:15:37 +0200

    API integration & polished code

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function updateWeather(response) {
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
   class="weather-app-icon"/>`;
 
-  getForecast(response.data, city); 
+  getForecast(response.data.city); 
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -53,11 +53,11 @@ function searchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-function formatDay(timestamp){
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat",];
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
-  return days [date.getDay()];
+  return days[date.getDay()];
 }
 function getForecast(city) {
   let apiKey = "4te9f58ab5b4c4cd8o33b106e27f5729";
@@ -68,18 +68,21 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
-    if (index < 5){
-    forecastHtml =
+    if (index < 5) {
+     forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
       <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
       <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
          <div class="weather-forecast-temperatures">
            <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+              )}°</div>
             </div>
             </div> 
       `;
